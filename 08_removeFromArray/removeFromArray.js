8046581897b623f6f315c793c4c8a55acb3b39d3
Lukas Prokopec <koki.prokopec@gmail.com>
2026-01-08 17:21:11 +0100

    completed excercises 6-11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...remove) {
+  let filteredArray = arr;
+    for (let i = 0; i < remove.length; i++) {
+    filteredArray = filteredArray.filter((element) => element !== remove[i]);
+  }
+  return filteredArray;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (Number.isInteger(a) === false || Number.isInteger(b) === false) {
+    return "ERROR";
+  } else if ((a || b) < 0) {
+    return "ERROR";
+  } else if (b < a) {
+    let c = a;
+    a = b;
+    b = c;
+  }
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
