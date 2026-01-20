fe60e91d63243b8ecf81bc108104187703848db0
Lukas Prokopec <koki.prokopec@gmail.com>
2026-01-20 16:57:28 +0100

    completed exercises 15-16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  // list of people with their age
+  const ages = people.map((person) => {
+    // if yearOfDeath missing
+    if (!person.yearOfDeath) {
+      return {
+        name: person.name,
+        age: new Date().getFullYear() - person.yearOfBirth,
+      };
+    }
+    return {
+      name: person.name,
+      age: person.yearOfDeath - person.yearOfBirth,
+    };
+  });
+  // picking the oldest person a returning his name
+  const oldest = ages.reduce((max, person) => {
+    if (person.age > max.age) {
+      return person;
+    } else {
+      return max;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
